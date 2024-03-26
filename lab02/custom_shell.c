47630d7f8c8979af665ee3a9e9b29bd5de6a331e
afmireski <afmireski@outlook.com>
2024-03-25 22:56:06 -0300

    Finished ex04 logic

--- lab02/custom_shell.c
+++ lab02/custom_shell.c
@@ -12,56 +12,72 @@ int main(){
     char* exitCommand = "exit";
     int maxArgumentsCount = 10;
     
-    char input[200];
-    printf("------------------|SHELL|------------------\n");
-    printf("Informe o comando que deseja executar. Ex: 'ls -al'\n");
-    printf("'exit' para sair\n");
-    printf("> ");
-    fgets(input, sizeof(input), stdin);
-    input[strcspn(input, "\n")] = '\0'; // Remove o caractere de nova linha
+    while (1) {
+        char input[200];
+        printf("------------------|SHELL|------------------\n");
+        printf("Informe o comando que deseja executar. Ex: 'ls -al'\n");
+        printf("*'exit' para sair\n");
+        printf("*Máximo de %d argumentos por comando\n", maxArgumentsCount);
+        printf("> ");
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = '\0'; // Remove o caractere de nova linha
 
-    char aux[100];
-    char delimiter[2] = " ";
-    strcpy(aux, input);
+        char aux[200];
+        char delimiter[2] = " ";
+        strcpy(aux, input);
 
-    // O primeiro token é o comando
-    char* token = strtok(aux, delimiter); 
-    if (strcmp(token, exitCommand) == 0) {
-        printf("Saindo...\n");
-        return 0;
-    }
-
-    char** arguments = (char**) malloc(maxArgumentsCount+1 * sizeof(char*));
-    arguments[0] = token;
-
-    int i;
-    for (i = 1; token != NULL && i <= maxArgumentsCount; i++) {
-        // Obtém os argumentos do comando;
-        token = strtok(NULL, delimiter);
-        arguments[i] = token;
+        // O primeiro token é o comando
+        char* token = strtok(aux, delimiter);
         
-    }
-    if (arguments[maxArgumentsCount] != NULL) arguments[maxArgumentsCount] = NULL;
+        // Verifica se o token é 'exit', ou, caso comando único, se ele é 'exit'.
+        if (strlen(token) == 0) {
+            if (strcmp(aux, exitCommand) == 0) {
+                printf("Saindo...\n");
+                break;
+            }
+            strcpy(token, aux);
+        } else if (strcmp(token, exitCommand) == 0) {
+            printf("Saindo...\n");
+            break;
+        }        
+       
+        char** arguments = (char**) calloc(maxArgumentsCount+1, sizeof(char*));
+        arguments[0] = token;
+        bool isBackground = false;
 
-    pid_t pid = fork();
-    if (pid == 0) {
-        printf("Sou o filho chamando exec\n");
-        int status = execvp(token, arguments);
-
-        printf("Status: %d\n", status);
-        if (status < 0) {
-            printf("Falha ao executar comando");
-            return 1;
+        int i;
+        for (i = 1; token != NULL && i < maxArgumentsCount; i++) {
+            // Obtém os argumentos do comando;
+            token = strtok(NULL, delimiter);
+            if (token != NULL) {
+                if (strcmp(token, "&") == 0) isBackground = true;
+            }
+            arguments[i] = token;
+            
         }
-    } else {
-        printf("Sou o pai\n");
-        // Se é o pai talvez espere o filho
-        wait(NULL);
-        free(arguments);
-        free(token);
-    }
+        if (arguments[maxArgumentsCount] != NULL) arguments[maxArgumentsCount] = NULL;
+        pid_t pid = fork();
+        if (pid == 0) {
+            int status = execvp(arguments[0], arguments);
 
-    printf("-------------------------------------------\n");
+            printf("Status: %d\n", status);
+            if (status < 0) {
+                printf("Falha ao executar comando");
+                return 1;
+            }
+        } else {
+            // É o pai
+            // Verifica se o comando do exec executará em segundo plano
+            if (!isBackground) {
+                // Se não, espera terminar
+                wait(NULL);
+                // Limpar a memória alocada manualmente
+                free(arguments);
+                free(token);
+            }
+        }
+        printf("-------------------------------------------\n");
+    }
 
     exit(0);
     return 0;
